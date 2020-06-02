00cb2a4540203b18ae17d262b898cdc03021d162
zhengyh <zhengyh@xitaiinfo.com>
2020-06-02 10:04:36 +0800

    项目代码初始提交

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="home">
+		<p>eslint空格终于可以了啊</p>
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+	name: "Home",
+	components: {
+		HelloWorld
+	},
+	data() {
+		return {
+			name: "eslint空格终于可以了啊"
+		};
+	}
+};
+</script>
+<style lang="scss" scoped>
+p {
+	color: #dddddd;
+	font-size: 18px;
+}
+</style>
