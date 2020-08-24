87560696c0b1dc85e513583208198209dc5aa0f7
zhengyh <zhengyh@xitaiinfo.com>
2020-08-24 16:54:50 +0800

    vscode编辑器提交测试

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
 		path: "/Main",
 		name: "Main",
 		component: () => import("../views/Main/index.vue"),
-		redirect:'/zzButton',
+		redirect: '/zzButton',
 		children: [
 			{
 				path: "/zzButton",
@@ -38,27 +38,27 @@ const routes = [
 				path: "/zzInput",
 				name: "zzInput",
 				component: () => import("../views/Main/input.vue")
-			},{
+			}, {
 				path: "/zzSelect",
 				name: "zzSelect",
 				component: () => import("../views/Main/select.vue")
-			},{
+			}, {
 				path: "/checkbox",
 				name: "checkbox",
 				component: () => import("../views/Main/checkbox.vue")
-			},{
+			}, {
 				path: "/zzCascader",
 				name: "zzCascader",
 				component: () => import("../views/Main/cascader.vue")
-			},{
+			}, {
 				path: "/zzDatePicker",
 				name: "zzDatePicker",
 				component: () => import("../views/Main/datePicker.vue")
-			},{
+			}, {
 				path: "/zzTransfer",
 				name: "zzTransfer",
 				component: () => import("../views/Main/transfer.vue")
-			},{
+			}, {
 				path: "/zzTabs",
 				name: "zzTabs",
 				component: () => import("../views/Main/tabs.vue")
@@ -66,23 +66,23 @@ const routes = [
 				path: "/zzLoading",
 				name: "zzLoading",
 				component: () => import("../views/Main/loading.vue")
-			},{
+			}, {
 				path: "/zzSlider",
 				name: "zzSlider",
 				component: () => import("../views/Main/slider.vue")
-			},{
+			}, {
 				path: "/zzNavMenu",
 				name: "zzNavMenu",
 				component: () => import("../views/Main/NavMenu.vue")
-			},{
+			}, {
 				path: "/zzDialog",
 				name: "zzDialog",
 				component: () => import("../views/Main/dialog.vue")
-			},{
+			}, {
 				path: "/zzCarousel",
 				name: "zzCarousel",
 				component: () => import("../views/Main/carousel.vue")
-			},{
+			}, {
 				path: "/zzAlert",
 				name: "zzAlert",
 				component: () => import("../views/Main/alert.vue")
